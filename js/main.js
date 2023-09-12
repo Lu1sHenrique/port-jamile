4d1c2ba8bf885910ea00c26598a34220d28334be
Henrique <lhenrique.lh000@gmail.com>
2023-09-11 22:08:08 -0300

    feat: ajuste port

--- js/main.js
+++ js/main.js
@@ -583,7 +583,7 @@ var contactForm = function() {
 		            }
 			      },
 			      error: function() {
-			      	$('#form-message-warning').html("Something went wrong. Please try again.");
+			      	$('#form-message-warning').html("Algo deu errado.");
 			         $('#form-message-warning').fadeIn();
 			         $submit.css('display', 'none');
 			      }
